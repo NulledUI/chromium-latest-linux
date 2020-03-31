9062b09a5a31bcd8706b164c57356799cc5c03f8
Marek Sebera <marek.sebera@gmail.com>
2020-03-31 09:49:57 +0200

    resolving correct path when run.sh is symlinked to another directory

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-BASEDIR=$(dirname $0)
+BASEDIR=$(dirname $(realpath $0))
 
 export CHROME_DEVEL_SANDBOX="$BASEDIR/latest/chrome_sandbox"
 $BASEDIR/latest/chrome --user-data-dir="$BASEDIR/user-data-dir" $* &> /dev/null &
